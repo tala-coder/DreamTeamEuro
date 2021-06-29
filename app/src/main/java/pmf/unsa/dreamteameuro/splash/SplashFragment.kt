241c968cc818c59b39f8da9e1ec434837029cf5d
Covid-imunizacija <82477363+Covid-imunizacija@users.noreply.github.com>
2021-06-30 00:46:58 +0200

    bottom navigation

--- app/src/main/java/pmf/unsa/dreamteameuro/splash/SplashFragment.kt
+++ app/src/main/java/pmf/unsa/dreamteameuro/splash/SplashFragment.kt
@@ -2,10 +2,10 @@ package pmf.unsa.dreamteameuro.splash
 
 import android.os.Bundle
 import android.os.Handler
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import pmf.unsa.dreamteameuro.R
 import pmf.unsa.dreamteameuro.databinding.FragmentSplashBinding
@@ -14,14 +14,28 @@ import pmf.unsa.dreamteameuro.databinding.FragmentSplashBinding
 class SplashFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
         val binding = FragmentSplashBinding.inflate(inflater)
 
+//        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
 
         Handler().postDelayed({
-                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
-         }, 3000)
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }, 3000)
 
         return binding.root
     }
+
+
+//    override fun onResume() {
+//        super.onResume()
+//        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+//    }
+
 }
