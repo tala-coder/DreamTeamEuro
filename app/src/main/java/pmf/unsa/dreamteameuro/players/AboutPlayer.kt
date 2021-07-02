8c53e6c31b045295bf79fe20dbb55dc1e94aac42
ibrahim <ibrahinmesan@gmail.com>
2021-07-02 20:02:28 +0200

    click listener

--- app/src/main/java/pmf/unsa/dreamteameuro/players/AboutPlayer.kt
+++ app/src/main/java/pmf/unsa/dreamteameuro/players/AboutPlayer.kt
@@ -10,19 +10,32 @@ import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.about_player_fragment.*
 import kotlinx.android.synthetic.main.about_player_fragment.view.*
+import kotlinx.android.synthetic.main.recyclerview_player_layout_database.view.*
 import pmf.unsa.dreamteameuro.R
 import pmf.unsa.dreamteameuro.data.Player
 import pmf.unsa.dreamteameuro.data.PlayerViewModel
 
 class AboutPlayer : Fragment() {
 
-
+    private lateinit var args: AboutPlayerArgs
     private lateinit var viewModel: PlayerViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        args = AboutPlayerArgs.fromBundle(requireArguments())
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.about_player_fragment, container, false)
         viewModel = ViewModelProvider(this).get(PlayerViewModel::class.java)
+
+        view.namePly.text = args.playerName
+        view.agePly.text = args.playerDate.toString()
+        view.positionPly.text = args.playerPosition
+        view.heightPly.text = args.playerHeight.toString()
+        view.valuePly.text = args.playerValue.toString()
+        view.numberPly.text = args.playerNumber.toString()
 
         view.add_floatingActionButton.setOnClickListener{
             insertPlayerToDatabase()
@@ -33,9 +46,9 @@ class AboutPlayer : Fragment() {
 
     private fun insertPlayerToDatabase() {
         // todo: promijeniti nazive
-        val t1 = t1.text.toString()
-        val t2 = t2.text.toString()
-        val t3 = t3.text.toString()
+        val t1 = namePly.text.toString()
+        val t2 = numberPly.text.toString()
+        val t3 = agePly.text.toString()
 //        val t4 = t4.text.toString()
 
 
